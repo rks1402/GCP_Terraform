84f034483513dbd8dbd3bff374d13d007a3b9948
Rahul Kumar Sharma <rahul.official2419@gmail.com>
2023-01-24 17:26:02 +0530

    added sql file

--- main.tf
+++ main.tf
@@ -30,3 +30,10 @@ resource "google_sql_database_instance" "instance" {
 
 }
 
+resource "google_sql_user" "users" {
+  name     = "rahul"
+  instance = google_sql_database_instance.instance.name
+  password = "rahul"
+  type = "BUILT_IN"
+}
+
