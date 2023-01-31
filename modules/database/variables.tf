a2ecbb32d8fc4665f9766cae40d98e799b4d2fbd
Rahul Kumar Sharma <rahul.official2419@gmail.com>
2023-01-31 13:10:50 +0530

    testing

--- modules/database/variables.tf
+++ modules/database/variables.tf
@@ -1,2 +1,8 @@
 variable "project" {}
 variable "subnet" {}
+variable "google_sql_database_instance" {
+    name = string
+    region = string
+    database_version = string
+    settings = object({tier = string})
+}
